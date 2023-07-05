d62d2862fd229f7d686a290470e1a7a66ff1d3f4
chrizzzii <clvtito@gmail.com>
2023-07-05 20:10:46 +0700

    Develop toggle todo feature

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import TodoItem from './TodoItem' // Lakukan import
+import React from "react";
+import TodoItem from "./TodoItem"; // Lakukan import
 
-const Todos = ({ todos }) => {
+// Menerima function toggleCompleted sebagai sebuah prop
+const Todos = ({ todos, toggleCompleted }) => {
   return (
     <div style={styles.container}>
       {todos.map((todo) => {
-        return <TodoItem key={todo.id} todo={todo} />
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            // Teruskan function toggleCompleted ke component TodoItem
+            toggleCompleted={toggleCompleted}
+          />
+        );
       })}
     </div>
-  )
-}
-
+  );
+};
 const styles = {
   container: {
-    width: '40%',
-    margin: '0 auto',
+    width: "40%",
+    margin: "0 auto",
   },
-}
+};
 
-export default Todos
+export default Todos;
